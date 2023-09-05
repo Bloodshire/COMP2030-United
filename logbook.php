52e6085ddcaf8f08d47bb3f9c14f31510d88c687
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-09-05 15:08:38 +0930

    added title

--- logbook.php
+++ logbook.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Total Learner Driver Record</title>
+        <title>Add Drive</title>
         <meta charset="utf-8">
         <meta name="Authors" content=" Callum and Michael">
         <link rel="stylesheet" href="styles/style.css">
