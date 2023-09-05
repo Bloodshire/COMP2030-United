effed0abe0bd16188e55c4fb71d638b1d8978415
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-09-05 15:55:46 +0930

    Styled nav bar and display block

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -1,10 +1,10 @@
 <div class="sidenav">
-    <h1>TLDR</h1>
+    <h1 class="nav-bar-title">TLDR</h1>
     <a href="#">Dashboard</a>
     <a href="#">Log Drive</a>
     <a href="#">Progress & Hours</a>
     <a href="#">Drive Summary</a>
     <a href="#">Map Review</a>
     <a href="#">Profile</a>
-    <a href="#">Logout</a>
+    <a href="#" class="bottom-aligned">Logout</a>
 </div>
