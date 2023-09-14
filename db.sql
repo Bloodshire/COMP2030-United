701bb8df926eeb2faad4a2f5d98ea4e2e4267988
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-09-14 15:44:19 +0930

    Updated DB

--- db.sql
+++ db.sql
@@ -9,12 +9,12 @@ CREATE TABLE Account(
     account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    accType VARCHAR(10) NOT NULL
+    accType TINYINT(1) NOT NULL
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE Logbook (
     log_id INT AUTO_INCREMENT PRIMARY KEY,
-    approved BIT DEFAULT 0,
+    approved TINYINT(1) DEFAULT 0,
     learner_id INT NOT NULL,
     QSD_id INT NULL,
     instructor_id INT NULL,
