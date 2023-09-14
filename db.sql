c575b489dbb425aa2c610a49afb1c117b4e27ce5
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-09-14 15:30:31 +0930

    Added to db.sql

--- db.sql
+++ db.sql
@@ -5,12 +5,57 @@ CREATE DATABASE TLDR;
 
 USE TLDR;
 
-CREATE TABLE Task(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(100),
-    completed boolean NOT NULL DEFAULT 0,
-    updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE Account(
+    account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    accType VARCHAR(10) NOT NULL
 ) AUTO_INCREMENT = 1;
+
+CREATE TABLE Logbook (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    approved BIT DEFAULT 0,
+    learner_id INT NOT NULL,
+    QSD_id INT NOT NULL,
+    date DATE NOT NULL,
+    startTime TIME NOT NULL,
+    finishTime TIME NOT NULL,
+    duration INT NOT NULL,
+    from_location VARCHAR(255) NOT NULL,
+    to_location VARCHAR(255) NOT NULL,
+    road_condition VARCHAR(255) NOT NULL,
+    weather_condition VARCHAR(255) NOT NULL,
+    traffic_condition VARCHAR(255) NOT NULL,
+    FOREIGN KEY (learner_id) REFERENCES Learner(learner_id),
+    FOREIGN KEY (QSD_id) REFERENCES QSD(QSD_id)
+);
+
+CREATE TABLE Instructor (
+    instructor_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT NOT NULL,
+    licenceNo VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES Account(account_id)
+);
+
+CREATE TABLE Learner (
+    learner_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT NOT NULL,
+    permitNo VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+CREATE TABLE QSD (
+    QSD_id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id INT NOT NULL,
+    licenceNo VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account(account_id) 
+);
 
 CREATE user IF NOT EXISTS dbadmin@localhost;
 GRANT all privileges ON Practical3.Task TO dbadmin@localhost;
