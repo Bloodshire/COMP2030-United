24868fcdb8afbf9ba02205e8297962d3abda9a53
Bloodshire <81276458+Bloodshire@users.noreply.github.com>
2023-09-14 15:20:18 +0930

    Begin working on Login page

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="Authors" content="Harrison">
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="scripts/script.js" defer></script>
+</head>
+
+<body id="loginBody">
+
+
+
+</body>
+
+</html>
