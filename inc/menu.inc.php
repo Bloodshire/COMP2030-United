26efd147216c151130c7a3b6a641036b30296ae9
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-09-14 15:47:21 +0930

    Changed Progress & Hours to Dashboard

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -6,7 +6,7 @@
     <li><a href="dashboard.php"><i class="fa-xl fa-solid fa-house"></i><br>Dashboard</a></li>
     <li><a href="logbook.php"><i class="fa-xl fa-solid fa-car-side"></i><br>Log Book</a></li>
     <li><a href="cbta.php"><i class="fa-xl fa-solid fa-book"></i><br>CBT&A</a></li>
-    <li><a href="progresshours.php"><i class="fa-xl fa-solid fa-chart-line"></i><br>Progress & Hours</a></li>
+    <!-- <li><a href="progresshours.php"><i class="fa-xl fa-solid fa-chart-line"></i><br>Progress & Hours</a></li> -->
     <li><a href="drivesummary.php"><i class="fa-xl fa-regular fa-clipboard"></i><br>Drive Summary</a></li>
     <li><a href="payments.php"><i class="fa-solid fa-xl fa-dollar-sign"></i><br>Payments</a></li>
     <li><a href="profile.php"><i class="fa-xl fa-solid fa-user"></i><br>Profile</a></li>
