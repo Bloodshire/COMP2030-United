3e375dceac9119e6f1e475762b734167c84eed5d
lilGoanna <49540390+lilGoanna@users.noreply.github.com>
2023-10-14 19:20:19 +1030

    Removed table 'pending_entries'
    Made redundant since "approvals" table does it's job.

--- db.sql
+++ db.sql
@@ -67,17 +67,6 @@ CREATE TABLE approvals (
     FOREIGN KEY (approver_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE pending_entries (
-    pending_entry_id INT AUTO_INCREMENT PRIMARY KEY,
-    student_id INT NOT NULL,
-    approver_id INT NOT NULL,
-    entry_date DATE NOT NULL,
-    activity_description TEXT NOT NULL,
-    submitted_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (student_id) REFERENCES users (user_id),
-    FOREIGN KEY (approver_id) REFERENCES users (user_id)
-);
-
 CREATE TABLE payments (
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
     instructor_id INT NOT NULL,
@@ -111,8 +100,8 @@ INSERT INTO users (email, password, given_name, surname, date_of_birth, street_a
 VALUES
     ('instructor1@example.com', '$2y$10$mrKQy6U46EC9cGdebUe7zeFsVoeG6yvC5.BJ/a1bTXXvKvXdDUx3G', 'Hans', 'Zimmer', '1980-01-15', '123 Main St', 'Hendon', 'CA', '90210', 'FN1023', 1),
     ('instructor2@example.com', '$2y$10$mrKQy6U46EC9cGdebUe7zeFsVoeG6yvC5.BJ/a1bTXXvKvXdDUx3G', 'Christopher', 'Nolan', '1975-07-30', '456 Elm St', 'Hendon', 'NY', '10001', 'FN7829', 1),
-    ('qsd1@example.com', '$2y$10$JvTv5GkQhn5TB89e23lapuqs/0cMzTW0lr6A8FgcUWMLYEq1ab.Ei', 'James', 'Cameron', '1982-03-22', '789 Oak St', 'TX', 'Hendon', '77002', 'FN2710', 2),
-    ('qsd2@example.com', '$2y$10$JvTv5GkQhn5TB89e23lapuqs/0cMzTW0lr6A8FgcUWMLYEq1ab.Ei', 'Michael', 'Bay', '1978-12-10', '101 Pine St', 'FL', 'Hendon', '33001', 'FN7291', 2);
+    ('qsd1@example.com', '$2y$10$JvTv5GkQhn5TB89e23lapuqs/0cMzTW0lr6A8FgcUWMLYEq1ab.Ei', 'James', 'Cameron', '1982-03-22', '789 Oak St', 'TX', 'Hendon', '77002', 'FN2710', 1),
+    ('qsd2@example.com', '$2y$10$JvTv5GkQhn5TB89e23lapuqs/0cMzTW0lr6A8FgcUWMLYEq1ab.Ei', 'Michael', 'Bay', '1978-12-10', '101 Pine St', 'FL', 'Hendon', '33001', 'FN7291', 1);
 
 INSERT INTO users (email, password, given_name, surname, date_of_birth, street_address, suburb, state, postcode, license_no, role_id, instructor_id)
 VALUES
